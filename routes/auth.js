0e4e2860715436a4f2e8a4ab24d2342a906807f2
MarcelaDomici <marceladomiciano2@gmail.com>
2025-04-22 23:18:37 -0300

    login user

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const { cadastrarUsuario } = require('../controllers/authController');
+const db = require('../database/db'); 
 
 router.post('/register', cadastrarUsuario);
 
 module.exports = router;
+
+// LOGIN
+router.post('/login', (req, res) => {
+    const { email, senha } = req.body;
+  
+    if (!email || !senha) {
+      return res.status(400).json({ mensagem: 'Email e senha são obrigatórios.' });
+    }
+  
+    db.get('SELECT * FROM usuarios WHERE email = ?', [email], (err, usuario) => {
+      if (err) {
+        return res.status(500).json({ mensagem: 'Erro ao buscar usuário.' });
+      }
+  
+      // Se não encontrar usuário ou senha estiver incorreta
+      if (!usuario || usuario.senha !== senha) {
+        return res.status(401).json({ mensagem: 'Email ou senha incorretos.' });
+      }
+  
+      // Login bem-sucedido
+      res.status(200).json({ mensagem: 'Login realizado com sucesso.' });
+    });
+  });
+  
+  
